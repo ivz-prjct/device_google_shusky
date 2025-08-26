67d5ed302c9be9618e9692ec6d4b3df20e4cb243
Iverz <asterixiverz@gmail.com>
2025-08-26 15:40:34 +0000

    shusky: initial bring up for AfterlifeOS 8.0
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- afterlife_husky.mk
+++ afterlife_husky.mk
@@ -6,28 +6,33 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit device configuration
-DEVICE_CODENAME := shiba
+DEVICE_CODENAME := husky
 DEVICE_PATH := device/google/shusky
-VENDOR_PATH := vendor/google/shiba
+VENDOR_PATH := vendor/google/husky
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 $(call inherit-product, device/google/zuma/lineage_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 8
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_NAME := afterlife_$(DEVICE_CODENAME)
+
+# Afterlife Flags
+AFTERLIFE_MAINTAINER := Iverz
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_ENABLE_BLUR := true
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2992
+TARGET_SCREEN_WIDTH := 1344
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="shiba-user 16 BP2A.250805.005 13691446 release-keys" \
-    BuildFingerprint=google/shiba/shiba:16/BP2A.250805.005/13691446:user/release-keys \
+    BuildDesc="husky-user 16 BP2A.250805.005 13691446 release-keys" \
+    BuildFingerprint=google/husky/husky:16/BP2A.250805.005/13691446:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
